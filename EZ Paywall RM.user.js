8561ee9c7ce8ed79c4e224b46daa59f21c93ad6a
tygamer1to7 <11811416+tygamer1to7@users.noreply.github.com>
2019-04-30 19:29:10 -0500

    Update EZ Paywall RM.user.js

--- EZ Paywall RM.user.js
+++ EZ Paywall RM.user.js
@@ -1,37 +1,44 @@
 // ==UserScript==
+// @run-at       default
 // @name         EZ Paywall RM
 // @namespace    https://github.com/tygamer1to7/EZ-Paywall-RM/
-// @version      0.6
+// @version      0.7
 // @downloadURL  https://raw.githack.com/tygamer1to7/EZ-Paywall-RM/master/EZ Paywall RM.user.js
 // @description  A useful tool.
 // @author       tygamer1to7
 // @match        https://cooking.nytimes.com/*
 // @match        https://washingtonpost.com/*
 // @match        https://nytimes.com/*
+// @match        https://www.cooksillustrated.com/*
 // @grant        none
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-    $("head").children().appendTo("body");
     $('script').remove();
-    $("head").remove();
-    $('noscript').remove();
     if (window.location.hostname == "cooking.nytimes.com")
     {
-        var elem = document.getElementsByClassName("nytc---modal-window---windowContainer");
-        elem[0].remove();
+        $(".nytc---modal-window---windowContainer").remove();
         var elem2 = document.getElementsByTagName("html")[0];
         function checkFlag() {
             if (elem2.className == "desktop viewport-large nytc---modal-window---noScroll") {
-            elem2.className = "desktop viewport-large";
+            $(".nytc---modal-window---noScroll").removeClass("nytc---modal-window---noScroll");
             var elem3 = document.getElementsByTagName("body")[0];
             elem3.className = "";
+            $("body").removeClass("logged-out");
             } else {
                 window.setTimeout(checkFlag, 100); /* this checks the flag every 100 milliseconds*/
             }
         }
         checkFlag();
     }
+    if (window.location.hostname == "www.cooksillustrated.com") {
+        $(".paywall__overlay").remove();
+        $(".blurred").removeClass("blurred");
+        $("div.recipe__placeholder").removeAttr("style");
+}
+    $("head").children().appendTo("body");
+    $("head").remove();
+    $('noscript').remove();
 })();
